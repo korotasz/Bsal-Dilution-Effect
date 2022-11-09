da1ba3071b529998eee995dfcbfc0560be07be06
Alexis Korotasz <60368008+korotasz@users.noreply.github.com>
2022-11-09 12:23:40 -0500

    Update dataCleaning.R

--- dataCleaning.R
+++ dataCleaning.R
@@ -1,17 +1,29 @@
 library(tidyverse)
 library(devtools)
 library(reshape2)
+library(janitor) # for reviewing duplicates
 library(codyn) # species synchrony
 library(lubridate)
 library(ncdf4) # package for netcdf manipulation
 library(rgdal) # package for geospatial analysis
+library(raster) # package for raster manipulation
+library(maptools) # package to create maps
+library(geodata) # cmip6; projected climate data
+library(gstat) 
 library(raster)
 library(maptools)
 library(geodata) # cmip6
 library(gstat)
 library(sp)
+library(fs) # construct relative paths to files/directories
 
 
+
+setwd('C:/Users/alexi/OneDrive/Documents/01_GradSchool/_DissertationWork/Chapter4/03_code')
+euram <- read.csv("euram.csv", header = T, encoding = "UTF-8")
+germany <- read.csv("germany.csv", header = T, encoding = "UTF-8")
+uk <- read.csv("uk.csv", header = T, encoding = "UTF-8")
+spain <- read.csv("spain.csv", header = T, encoding = "UTF-8")
 setwd('C:/Users/alexi/OneDrive/Documents/01_GradSchool/_Dissertation work/Chapter4/03_code')
 euram <- read.csv("euramv2.csv", header = T, encoding = "UTF-8")
 germany <- read.csv("germanyv2.csv", header = T, encoding = "UTF-8")
@@ -36,13 +48,18 @@ prev$country <- gsub(prev$country, pattern = "Italy",
 
 ## Add empty columns for climactic variables/clean up the df a bit
 newcols <- c("ADM0", "ADM1", "ADM2", "susceptibility", "scientific", "nativeStatus",
-             "soilMoisture", "tmin", "tmax", "prec", "bio1", "bio2", "bio3", "bio4", 
+             "tmin", "tmax", "prec", "bio1", "bio2", "bio3", "bio4",
+             "soilMoisture", "tmin", "tmax", "prec", "bio1", "bio2", "bio3", "bio4",
              "bio5", "bio6", "bio7", "bio8", "bio9", "bio10", "bio11", "bio12", 
              "bio13", "bio14", "bio15","bio16", "bio17", "bio18", "bio19")
 prev[newcols] <- NA 
 names(prev)[names(prev) == 'specificEpithet'] <- 'species' # change name of this column
 prev$scientific <- paste(prev$genus, prev$species) # combine genus & species 
 data.frame(colnames(prev)) # returns indexed data frame 
+
+prev <- prev[, c(2, 34:36, 4:5, 21:23, 6, 3, 18:19, 8:9, 38, 37, 39,  # reorder columns to make more intuitive
+                 26:30, 20, 10:17, 7, 24, 40:61, 25, 1, 31:33)] 
+
 prev <- prev[, c(2, 34:36, 4:5, 21:23, 40, 6, 3, 18:19, 8:9, 38, 37, 39,  # reorder columns to make more intuitive
                  26:30, 20, 10:17, 7, 24, 41:62, 25, 1, 31:33)] 
 
@@ -51,10 +68,15 @@ spain_cols <- c("locationRemarks", "occurrenceRemarks", "principalInvestigator",
                 "eventRemarks", "expeditionCode", "projectId")
 spain[spain_cols] <- NA
 
+
 ## Rearrange columns to match main df
 spain <- spain[, c(2:7, 59, 8:9, 32, 1, 10:15, 17, 16, 30:31, 60, 28:29, 27, 
                    19, 21, 20, 22:26, 18, 62, 33:55, 61, 56, 57:58)]
 
+## Rearrange columns to match main df
+spain <- spain[, c(2:7, 59, 8:9, 32, 1, 10:15, 17, 16, 30:31, 60, 28:29, 27, 
+                   19, 21, 20, 22:26, 18, 62, 33:55, 61, 56, 57:58)]
+spain <- spain[, -10]
 ## Add Spain
 prev <- rbind(prev, spain)
 
@@ -78,6 +100,7 @@ prev <- prev %>%
 #          row.names = FALSE)
 
 ## Read in attribute table with ADM data and case match ADM levels in prev
+a <- read.csv("locations_with_ADMs.csv", header = T, encoding = "UTF-8")
 a <- read.csv("location_ADMs.csv", header = T, encoding = "UTF-8")
 
 a2 <- a %>%
@@ -143,7 +166,7 @@ spr <- prev %>%
   group_by(Site, date) %>% 
   summarise_all(sum) %>%
   ungroup() %>%
-  mutate(Richness = apply(.[,3:(ncol(.)-1)] > 0, 1, sum))
+  mutate(richness = apply(.[,3:(ncol(.)-1)] > 0, 1, sum))
 
 
 ## Calculate abundance of individual spp at a site during each sampling event
@@ -176,7 +199,7 @@ names(diversity)[names(diversity) == 'Shannon'] <- 'Diversity'
 ## Add community diversity (Shannon's Index) back into prev df 
 prev <- prev %>%
   left_join(diversity, by = c("Site", "date")) %>%
-  mutate(alphadiv = Diversity)
+  rename(alphadiv = "Diversity")
 
 
 ## Make sure columns that have categorical data are uniform in coding
@@ -209,12 +232,17 @@ weather <- prev %>%
   unite(decimalLatitude, decimalLongitude, sep = ", ", col = "LatLon", remove = F) %>%
   relocate(LatLon, .after = decimalLongitude) %>%
   dplyr::filter(!(dayCollected == "NA")) %>%
+  mutate(temp = NA, soilMoisture = NA) %>%
+  group_by(LatLon, date) %>%
+  unique() %>%
+  ungroup() %>%
   mutate(t0_temp = NA, t0_soilMoisture = NA, t0_precip = NA,
          t1_temp = NA, t1_soilMoisture = NA, t1_precip = NA,
          t2_temp = NA, t2_soilMoisture = NA, t2_precip = NA) %>%
   group_by(LatLon, date)
 
 weather <- unique(weather)
+
 weather$date <- as.Date(weather$date, format = "%Y-%m-%d")
 
 ## get dates for 1 month prior to sample date
@@ -225,6 +253,26 @@ for(i in 1:nrow(weather)){
   weather[i,6] <- as.Date(weather$date[i], format = "%Y-%m-%d") - days(60)
 }
 
+# Add dates t-1 and t-2 back into prev dataframe
+prev <- prev %>%
+  mutate(date = base::as.Date(date, "%Y-%m-%d")) %>%
+  left_join(weather[, c(1:2, 4:6)], by = c("decimalLatitude", "decimalLongitude", "date")) %>%
+  relocate(c("date_t1", "date_t2"), .after = date)
+
+
+weather2 <- weather %>%
+  group_by(decimalLatitude, decimalLongitude) %>%
+  pivot_longer(cols = c(date, date_t1, date_t2),
+               names_to = "timepoint",
+               values_to = "date") %>%
+  ungroup() %>%
+  dplyr::select(-c("LatLon", "yearCollected", "monthCollected", "dayCollected")) %>%
+  relocate(c("timepoint", "date"), .after = "decimalLongitude") %>%
+  unite(decimalLatitude, decimalLongitude, sep = ", ", col = "LatLon", remove = F)
+
+
+## Export to use in Python and PyQGIS to obtain weather data
+#write.csv(weather2, 'C:/Users/alexi/OneDrive/Documents/01_GradSchool/_Dissertation work/Chapter4/03_code/weather.csv',
 
 sum(is.na(weather$date)) # 0
 sum(is.na(weather$date_t1)) # 0
@@ -248,9 +296,71 @@ weather <- weather %>%
 ## Python 3.9.4 used to download .nc4 files from NASA's EarthData data repository for each date and location.
 
 ## Import temperature & soil moisture data from NASA's EarthData website (citation below)
+   ## Li, B., H. Beaudoing, and M. Rodell, NASA/GSFC/HSL (2020), GLDAS Catchment Land Surface Model L4 daily 0.25 x 0.25 degree GRACE-DA1 V2.2, 
+   ## Greenbelt, Maryland, USA, Goddard Earth Sciences Data and Information Services Center (GES DISC), Accessed: 2022-09-08.
+gldas <- read.csv("weather_merged.csv", header = T, encoding = "UTF-8")
+
+gldas <- gldas %>%
+  unite(c("yearCollected", "monthCollected", "dayCollected"), sep = "-", col = "date", remove = F) %>%
+  rename(soilMoisture = "SOILMOIST_kgm.21") %>%
+  rename(temp = "SURFTEMP_K1") %>%
+  dplyr::select(decimalLatitude, decimalLongitude, timepoint, date, soilMoisture, temp) %>%
+  unite(decimalLatitude, decimalLongitude, sep = ", ", col = "LatLon", remove = F) 
+
+
+# Copy separate temp and soilMoisture
+gldas2 <- gldas
+gldas2 <- gldas[!(is.na(gldas2$temp)|gldas$temp ==""),]
+
+# Add temp data back into gldas df
+gldas$temp = gldas2$temp[base::match(paste(gldas$decimalLatitude, gldas$decimalLongitude, gldas$date),
+                                     paste(gldas2$decimalLatitude, gldas2$decimalLongitude, gldas2$date))]
+
+gldas <- gldas %>%
+  na.omit() %>%
+  mutate(row = row_number()) %>%
+  mutate(date = as.Date(date, format = "%Y-%m-%d")) %>%
+  dplyr::group_by(decimalLatitude, decimalLongitude, timepoint) %>%
+  pivot_wider(names_from = timepoint, values_from = date)
+  
+  
+# separate data by timepoint
+gldas_date <- gldas %>% # 809 entries -- some missing
+  dplyr::select(LatLon, decimalLatitude, decimalLongitude, soilMoisture, temp, date) %>%
+  na.omit
+gldas_date_t1 <- gldas %>% # 804 entries -- some missing
+  dplyr::select(LatLon, decimalLatitude, decimalLongitude, soilMoisture, temp, date_t1) %>%
+  na.omit
+gldas_date_t2 <- gldas %>% # 805 entries -- some missing
+  dplyr::select(LatLon, decimalLatitude, decimalLongitude, soilMoisture, temp, date_t2) %>%
+  na.omit
+
+## Add back into weather df to prepare to left_join back into prev df
+weather <- weather[, -c(7:11)]
+weather <- weather %>%
+  dplyr::mutate(soilMoisture_date = NA, soilMoisture_date_t1 = NA, soilMoisture_date_t2 = NA,
+                temp_date = NA, temp_date_t1 = NA, temp_date_t2 = NA)
+
+# Soil Moisture (kg/m^2)
+weather$soilMoisture_date = gldas_date$soilMoisture[base::match(paste(weather$LatLon, weather$date),
+                                                           paste(gldas_date$LatLon, gldas_date$date))]
+weather$soilMoisture_date_t1 = gldas_date_t1$soilMoisture[base::match(paste(weather$LatLon, weather$date_t1),
+                                                              paste(gldas_date_t1$LatLon, gldas_date_t1$date_t1))]
+weather$soilMoisture_date_t2 = gldas_date_t2$soilMoisture[base::match(paste(weather$LatLon, weather$date_t2),
+                                                                      paste(gldas_date_t2$LatLon, gldas_date_t2$date_t2))]
+
+# Temperature (K)
+weather$temp_date = gldas_date$temp[base::match(paste(weather$LatLon, weather$date),
+                                      paste(gldas_date$LatLon, gldas_date$date))]
+weather$temp_date_t1 = gldas_date_t1$temp[base::match(paste(weather$LatLon, weather$date_t1),
+                                      paste(gldas_date_t1$LatLon, gldas_date_t1$date_t1))]
+weather$temp_date_t2 = gldas_date_t2$temp[base::match(paste(weather$LatLon, weather$date_t2),
+                                      paste(gldas_date_t2$LatLon, gldas_date_t2$date_t2))]
+
+
+
 ## Li, B., H. Beaudoing, and M. Rodell, NASA/GSFC/HSL (2020), GLDAS Catchment Land Surface Model L4 daily 0.25 x 0.25 degree GRACE-DA1 V2.2, 
 ## Greenbelt, Maryland, USA, Goddard Earth Sciences Data and Information Services Center (GES DISC), Accessed: 2022-09-08.
-
 
 
 
@@ -262,40 +372,73 @@ weather <- weather %>%
 ##10.5067/GPM/IMERGDL/DAY/06
 
 
+# Create file path to where nc4 files are at (Work in progress)
+#imerg_path <- path('E:/', '01_GradSchool', '_DissertationWork', 'Chapter4', '03_code', 'Weather','sample_data')
+
+# 
 
 
+#imerg <- nc_open('gimms3g_ndvi_1982-2012.nc4')
+# Save the print(nc) dump to a text file
+#{
+#  sink('gimms3g_ndvi_1982-2012_metadata.txt')
+#  print(nc_data)
+#  sink()
+#}
+
+## Add weather data to main dataframe
+prev$soilMoisture_date = weather$soilMoisture_date[base::match(paste(prev$decimalLatitude, prev$decimalLongitude, prev$date),
+                                                               paste(weather$decimalLatitude, weather$decimalLongitude, weather$date))]
+prev$soilMoisture_date_t1 = weather$soilMoisture_date_t1[base::match(paste(prev$decimalLatitude, prev$decimalLongitude, prev$date_t1),
+                                                                     paste(weather$decimalLatitude, weather$decimalLongitude, weather$date_t1))]
+prev$soilMoisture_date_t2 = weather$soilMoisture_date_t2[base::match(paste(prev$decimalLatitude, prev$decimalLongitude, prev$date_t2),
+                                                                     paste(weather$decimalLatitude, weather$decimalLongitude, weather$date_t2))]
+
+prev$temp_date = weather$temp_date[base::match(paste(prev$decimalLatitude, prev$decimalLongitude, prev$date),
+                                                               paste(weather$decimalLatitude, weather$decimalLongitude, weather$date))]
+prev$temp_date_t1 = weather$temp_date_t1[base::match(paste(prev$decimalLatitude, prev$decimalLongitude, prev$date_t1),
+                                                                     paste(weather$decimalLatitude, weather$decimalLongitude, weather$date_t1))]
+prev$temp_date_t2 = weather$temp_date_t2[base::match(paste(prev$decimalLatitude, prev$decimalLongitude, prev$date_t2),
+                                                                     paste(weather$decimalLatitude, weather$decimalLongitude, weather$date_t2))]
 
 
+prev <- prev[, -c(7:9, 24, 37)]
+prev <- prev[, c(1:6, 60, 11:13, 8:10, 7, 14:32, 61:70, 33:59)]
+
+# Convert temp from K to C
+prev$temp_date <- prev$temp_date - 273.15
+prev$temp_date_t1 <- prev$temp_date_t1 - 273.15
+prev$temp_date_t2 <- prev$temp_date_t2 - 273.15
 
 
-
-
+# Get rid of climate data for now
+#prev <- prev[, -c(46:66)]
 
 
 #### cbind model df
 ## Create two new columns for Bsal detection successes/failures at each site, 
 ## for each species, during each sampling event
-#prev$genus <- gsub("[[:space:]]", "", prev$genus) # get rid of weird spaces in this column
+prev$genus <- gsub("[[:space:]]", "", prev$genus) # get rid of weird spaces in this column
 
-#disease <- prev %>%
-#  dplyr::select(country, decimalLatitude, decimalLongitude, Site, sppAbun, siteAbun,
-#                richness, alphadiv, date, yearCollected, monthCollected, dayCollected,
-#                genus, species, scientific, susceptibility, diseaseDetected, tmin, tmax, 
-#                prec, bio1, bio2, bio3, bio4, bio5, bio6, bio7, bio8, bio9, bio10, bio11, 
-#                bio12, bio13, bio14, bio15, bio16, bio17, bio18, bio19) %>%
-#  group_by(Site, date, scientific) %>%
-#  mutate(NoBsal = sum(diseaseDetected == 0)) %>%
-#  mutate(YesBsal = sum(diseaseDetected == 1)) %>%
-#  distinct()
+disease <- prev %>%
+  dplyr::select(country, decimalLatitude, decimalLongitude, Site, yearCollected, 
+                monthCollected, dayCollected, date, date_t1, date_t2,  
+                genus, species, scientific, susceptibility, BdDetected, BsalDetected, fatal, 
+                sppAbun, siteAbun, richness, alphadiv, 
+                soilMoisture_date, soilMoisture_date_t1, soilMoisture_date_t2, 
+                temp_date, temp_date_t1, temp_date_t2) %>%
+  group_by(Site, date, scientific) %>%
+  distinct()
 
 
-#data.frame(colnames(disease))
-#disease <- disease[,c(1:17, 40:41, 18:39)]
+data.frame(colnames(disease))
 
-## File for cbind model:
-#write.csv(disease, 'C:/Users/alexi/OneDrive/Documents/01_GradSchool/_Dissertation work/Chapter4/03_code/bsal_analysis.csv',
+## File for final prev dataframe:
+#write.csv(prev, 'C:/Users/alexi/OneDrive/Documents/01_GradSchool/_DissertationWork/Chapter4/03_code/bsalData_clean.csv',
 #          row.names = FALSE)
 
-
+## File for cbind model:
+#write.csv(disease, 'C:/Users/alexi/OneDrive/Documents/01_GradSchool/_DissertationWork/Chapter4/03_code/bsalData_cbind.csv',
+#          row.names = FALSE)
 
 
